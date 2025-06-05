b6bab74822be7fe84927e49d4c754476bdb4cf0b
KillerZeus666 <katheryng2017@gmail.com>
2025-06-05 17:10:14 -0500

    Agregando componente Contador

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import { useState } from 'react';
 
@@ -22,7 +24,17 @@ function App() {
           src={freeCodeCampLogo}
           alt='Logo de freeCodeCamp' />
       </div>
-
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
     </div>
   );
 }
